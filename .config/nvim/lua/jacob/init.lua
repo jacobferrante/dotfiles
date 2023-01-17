4cbe0c77f9485144e8a2bda597ec7e6bd1989f0f
Jacob Ferrante <jacobferrante@protonmail.com>
2023-01-17 17:26:37 -0500

    update, been a while

---
+++ .config/nvim/lua/jacob/init.lua
@@ -0,0 +1,3 @@
+require("jacob.set")
+
+
---
+++ .config/nvim/lua/jacob/set.lua
@@ -0,0 +1,23 @@
+
+-- gruvbox
+-- vim.o.background = "dark" -- or "light" for light mode
+-- vim.cmd([[colorscheme gruvbox]])
+
+-- catappuccin
+vim.cmd.colorscheme "catppuccin-frappe"
+
+vim.opt.guicursor = ""
+
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
